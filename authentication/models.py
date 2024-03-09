e2e54d3e3994b5a2cf595b79dd956662aad3f5fb
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-03-09 11:02:23 -0600

    feat: Add User model and create migrations

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
 
 # Create your models here.
+
+class User(AbstractUser):
+    # Agrega related_name para evitar conflictos
+    groups = models.ManyToManyField(Group, related_name='user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='user_permissions')
+    objects: UserManager = UserManager()
