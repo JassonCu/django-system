8c5df379a25a69e6bfce002f5daadc13bfed2a33
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-03-09 12:49:47 -0600

    change: The serializer.py file is modified to use the default Django class

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
-from .models import User
-
+from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'full_name', 'is_active', 'date_joined', 'password']
+        fields = ['id', 'username','email', 'is_active', 'date_joined', 'password']
