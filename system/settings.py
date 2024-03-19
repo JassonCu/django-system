93bc1789239c864607e778f975f2cee4971b430e
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-03-19 14:04:41 -0600

    fix: unused mudule Csv

--- system/settings.py
+++ system/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config, Csv
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
