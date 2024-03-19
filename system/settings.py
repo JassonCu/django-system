0be1b2de15e36d1fb3b749c4e08d8b631faa5fba
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-03-19 14:54:33 -0600

    fix: bab static config

--- system/settings.py
+++ system/settings.py
@@ -127,12 +127,13 @@ REST_FRAMEWORK = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "system/static")
 ]
 
 # Configuraciones de media
