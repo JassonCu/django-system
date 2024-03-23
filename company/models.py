599537cd86e5f3381d747568d513698acf8b5495
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-03-23 09:34:17 -0600

    add Role class and fixture

--- company/models.py
+++ company/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .managers import *
 
 # Create your models here.
+class Role(models.Model):
+    """
+    Modelo para roles de empresas (ej. Doctor, Enfemera)
+    """
+    objects = RoleManager()
+    name = models.CharField(_('Nombre'), max_length=50)
+    name_spanish = models.CharField(_('Nombre Español'), max_length=50, default='')
+    description = models.CharField(_('Descripción'), max_length=150)
+    def __str__(self):
+        return self.name
